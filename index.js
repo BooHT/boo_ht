ad6734be6964d48ebe279fe6bf53965dd054837f
deming16 <deminglindm@gmail.com>
2020-02-23 12:19:26 -0800

    add mongoose boiler plate

--- index.js
+++ index.js
@@ -24,6 +24,19 @@
 // const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const PAGE_ACCESS_TOKEN = 'EAAgBNNw4zGABAFLAIrd9iIkpXAesz1kKdERl94THZCrzYo28cnFGgrZB2lZC4tdemKkd7EeNT0QMUCcxtulyOWAKRerXQNSBZCMnnGF2A8sZBGZBXr44Fjjzc2KATRKu5MVeSToe7QrGYDAAtZCjovRYKMN7akwqFMZBA4Qy6hsg0AZDZD';
 
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // Imports dependencies and set up http server
 const 
   request = require('request'),
