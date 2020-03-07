8079d5cd91d0c308e092dbeb522fe8842c4a8eca
leongshengmin <shengmin.leong@upguard.com>
2020-03-07 01:44:24 -0800

    bump node.

--- index.js
+++ index.js
@@ -136,40 +136,63 @@ async function handleMessage(sender_psid, received_message) {
       "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     }
   } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
+    
+    // if user shares location 
+    let lat, lng = null;
+    if(received_message.attachments[0].payload.coordinates)
+    {
+        console.log(received_message.attachments[0].payload.coordinates)
+        lat = received_message.attachments[0].payload.coordinates.lat;
+        lng = received_message.attachments[0].payload.coordinates.long;
 
-    console.log("ATTACHMENT URL: " + attachment_url)
-    if (attachment_url) {
-    gcloud_cli.upload(attachment_url);
+        const newMessage = new Message({
+          senderId: sender_psid,
+          messageText: 'lat=${lat},lng=${lng}',
+          attachmentUrl: ''
+        })
+        await newMessage.save()
 
-    }
+        response = {
+          "text": `You shared your location ${lat}, ${lng}`
+        } 
+    } else {
 
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
+      // if user shares image / other media types
+      // Get the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+      console.log("ATTACHMENT URL: " + attachment_url)
+
+      if (attachment_url) {
+      gcloud_cli.upload(attachment_url);
+      }
+
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Is this the right picture?",
+              "subtitle": "Tap a button to answer.",
+              "image_url": attachment_url,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Yes!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "No!",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
         }
       }
     }
+
   } 
   
   // Send the response message
