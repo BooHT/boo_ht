5ac69dcd85d1b3ba465da2f0c2e04ca985ff7371
Daniel Tan <danieltanjiawang@gmail.com>
2020-03-07 01:31:20 -0800

    update get_grid.py script

--- scripts/get_grids.py
+++ scripts/get_grids.py
@@ -1,6 +1,9 @@
 import json
 import requests
 
+URL_QUERY_START_SUBSTR = 'https://services5.arcgis.com/54falWtcpty3V47Z/arcgis/rest/services/general_offenses_year2/FeatureServer/0/query?where='
+URL_QUERY_END_SUBSTR = '&outFields=*&outSR=4326&f=json'
+
 response = requests.get("https://services5.arcgis.com/54falWtcpty3V47Z/arcgis/rest/services/general_offenses_year2/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
 
 offense_categories = [cat.lower() for cat in ["DANGEROUS DRUG", "HEALTH/SAFETY", "SAC CITY CODE", "OBSTRUCTING", "FRAUD"]]
@@ -8,16 +11,12 @@ offense_categories = [cat.lower() for cat in ["DANGEROUS DRUG", "HEALTH/SAFETY",
 json_data = json.loads(response.text)
 crime_list = json_data["features"]
 filtered_crime = []
-filtered_crime_string = "UPPER(GRID)%20like%20%25%27"
+filtered_crime_string = ""
 
 for crime in crime_list:
     if crime["attributes"]["Offense_Category"].lower() in offense_categories:
+        grid_num = crime["attributes"]["Grid"]
         # filtered_crime.append(crime["attributes"]["Grid"] + "%25'%20OR%20UPPER(Grid)%20like%20'%25")
-        filtered_crime_string += crime["attributes"]["Grid"] + "%25%27%20OR%20UPPER(GRID)%20like%20%27%25"
+        filtered_crime_string += f'UPPER(Grid)%20like%20%27%25{grid_num}%25%27%20OR%20'
 
-print(filtered_crime_string)
-
-
-# UPPER(Grid)%20like%20'%25123%25'%20OR%20UPPER(Grid)%20like%20'%25224%25'
-
-# https://services5.arcgis.com/54falWtcpty3V47Z/arcgis/rest/services/POLICE_GRIDS/FeatureServer/0/query?where=UPPER(GRID)%20like%20'%25123%25'&outFields=*&outSR=4326&f=json
+print(f'{URL_QUERY_START_SUBSTR}{filtered_crime_string[:-8]}{URL_QUERY_END_SUBSTR}')
